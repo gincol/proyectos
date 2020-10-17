7bb82e575b7c081d94f23ab50a8e44c05e76522e
ginés collado <gines.collado@gmail.com>
2020-10-17 11:01:42 +0200

    actualizacion

---
+++ java/workareas/workspace-arq/sb-owasp/src/main/java/cat/vn/owasp/config/WebSecurityConfig.java
@@ -0,0 +1,75 @@
+package cat.vn.owasp.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+import cat.vn.owasp.config.owasp.MyDisableUrlSessionFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    private MyDisableUrlSessionFilter myDisableUrlSessionFilter;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//super.configure(http);
+		
+		http
+			.addFilterBefore(myDisableUrlSessionFilter,UsernamePasswordAuthenticationFilter.class)
+			.authorizeRequests()
+				.antMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll()
+			;
+//		http
+//        .csrf().disable()
+//        .authorizeRequests().anyRequest().permitAll()
+//        .and()
+//        .httpBasic();
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+    @Bean
+    public HttpFirewall looseHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);
+        firewall.setAllowUrlEncodedSlash(true);
+        firewall.setAllowBackSlash(true);
+        firewall.setAllowUrlEncodedPercent(true);
+        firewall.setAllowUrlEncodedPeriod(true);
+        return firewall;
+    }
+}
